96ac9d752d512355301a12904af0cbdb82beb581
BadNameOk <jjpocharatf2@gmail.com>
2023-02-02 23:03:23 +0700

    added controllers

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,27 +1,9 @@
 const express = require("express")
+const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital} = require('../controllers/hospitals')
 
-router = express.Router()
+const router = express.Router()
 
-const app = express();
+router.route('/').get(getHospitals).post(createHospital)
+router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital)
 
-router.get('/',(req, res) => {
-    res.status(200).json({ success: true, msg:'Show all hospitals'});
-});
-
-router.get('/:id',(req, res) => {
-    res.status(200).json({ success: true, msg:`Show hospitals ${req.params.id}`});
-});
-
-router.post('/',(req, res) => {
-    res.status(200).json({ success: true, msg:'Create new hospitals'});
-});
-
-router.put('/:id',(req, res) => {
-    res.status(200).json({ success: true, msg:`Update hospital ${req.params.id}`});
-});
-
-router.delete('/:id',(req, res) => {
-    res.status(200).json({ success: true, msg:`Delete hospital ${req.params.id}`});
-});
-
-module.exports = router;
+module.exports = router
