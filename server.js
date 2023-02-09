9593ceb067d67ffcb0d3e839d788256b15792239
BadNameOk <jjpocharatf2@gmail.com>
2023-02-09 22:49:49 +0700

    added models

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
+
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
 const app = express();
+app.use(express.json());
+
 
 const hospitals = require('./routes/hospitals');
 app.use(`/api/v1/hospitals`, hospitals);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
