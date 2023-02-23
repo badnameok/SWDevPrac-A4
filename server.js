0d2838028ba9f75d1ad810b1f9e7f3d53088b632
BadNameOk <jjpocharatf2@gmail.com>
2023-02-23 21:24:17 +0700

    added authentication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
@@ -10,9 +11,13 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.use(cookieParser());
+
 
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
 app.use(`/api/v1/hospitals`, hospitals);
+app.use(`/api/v1/auth`, auth);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
