2e0837f6d5cd7ef99ba670c2b413b9dc76e80007
BadNameOk <jjpocharatf2@gmail.com>
2023-03-16 23:23:16 +0700

    Assignment 7 : Relationships

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// npm run dev
+
 const express = require('express');
 
 const dotenv = require('dotenv');
@@ -15,8 +17,10 @@ app.use(cookieParser());
 
 
 const hospitals = require('./routes/hospitals');
+const appointments = require('./routes/appointments');
 const auth = require('./routes/auth');
 app.use(`/api/v1/hospitals`, hospitals);
+app.use(`/api/v1/appointments`, appointments);
 app.use(`/api/v1/auth`, auth);
 
 const PORT = process.env.PORT || 5000;
